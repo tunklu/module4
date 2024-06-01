54cba2a32e51ade3c6c8b99933e249a1900e45f0
Denis <vin_ogr@list.ru>
2024-06-01 13:53:36 +0700

    task модули и пакеты

---
+++ modules and packages/fake_math.py
@@ -0,0 +1,4 @@
+def fake_divide(first, second):
+    if 0 == second:
+        return ('Ошибка')
+    return first/second
---
+++ modules and packages/true_math.py
@@ -0,0 +1,7 @@
+import math
+from math import inf
+
+def true_divide(first, second):
+    if 0 == second:
+        return math.inf
+    return first/second
